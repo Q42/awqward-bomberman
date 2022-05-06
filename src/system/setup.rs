d802f4571f6eb34606e88aa45f396311299db269
djohalo2 <djovanni@q42.nl>
2022-05-06 10:48:50 +0200

    Load and render texture atlas sprite

--- src/system/setup.rs
+++ src/system/setup.rs
@@ -1,9 +1,16 @@
 use bevy::{prelude::*};
 
-pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut texture_atlases: ResMut<Assets<TextureAtlas>>,) {
+  let texture_handle = asset_server.load("sprites/stage.png");
+  let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(16.0, 16.0), 3, 3);
+  let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
   commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-  commands.spawn_bundle(SpriteBundle {
-      texture: asset_server.load("sprites/stage.png"),
-      ..default()
-  });
+  commands
+      .spawn_bundle(SpriteSheetBundle {
+          texture_atlas: texture_atlas_handle,
+          sprite: TextureAtlasSprite::new(8),
+          transform: Transform::from_scale(Vec3::splat(2.0)),
+          ..default()
+      });
 }
