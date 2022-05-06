b9501620905424123fb6d0b0c23e0e22ed2c0f79
djohalo2 <djovanni@q42.nl>
2022-05-06 10:30:16 +0200

    Show stage

--- src/system/setup.rs
+++ src/system/setup.rs
@@ -3,7 +3,7 @@ use bevy::{prelude::*};
 pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
   commands.spawn_bundle(OrthographicCameraBundle::new_2d());
   commands.spawn_bundle(SpriteBundle {
-      texture: asset_server.load("sprites/bomberman.png"),
+      texture: asset_server.load("sprites/stage.png"),
       ..default()
   });
 }
