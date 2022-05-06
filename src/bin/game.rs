17bb090b738af1838fac2c9f87af7980b7e6f264
Wesley Klop <wesley@q42.nl>
2022-05-06 15:56:47 +0200

    Add gamepad debug info

--- src/bin/game.rs
+++ src/bin/game.rs
@@ -1,10 +1,8 @@
 use awqward_bomberman::{
     models::player::Action,
     system::{
-        movement::move_player_system,
+        explode_bomb::*, gamepad::gamepad_system, movement::move_player_system, place_bomb::*,
         setup::*,
-        explode_bomb::*,
-        place_bomb::*
     },
     TIME_STEP,
 };
@@ -17,10 +15,9 @@ use leafwing_input_manager::plugin::InputManagerPlugin;
 fn main() -> Result<(), Report> {
     color_eyre::install()?;
 
-
     App::new()
         .insert_resource(WindowDescriptor {
-            title: "Bomberman".to_string(),
+            title: "Awqward bomberman".to_string(),
             width: awqward_bomberman::WINDOW_WIDTH,
             height: awqward_bomberman::WINDOW_HEIGHT,
             scale_factor_override: Some(4.0),
@@ -41,8 +38,9 @@ fn main() -> Result<(), Report> {
                 .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
                 .with_system(move_player_system)
                 .with_system(explode_bomb)
-                .with_system(place_bomb)
+                .with_system(place_bomb),
         )
+        .add_system(gamepad_system)
         .run();
 
     Ok(())
--- src/system/mod.rs
+++ src/system/mod.rs
@@ -1,4 +1,5 @@
 pub mod explode_bomb;
+pub mod gamepad;
 pub mod movement;
 pub mod setup;
 pub mod place_bomb;
