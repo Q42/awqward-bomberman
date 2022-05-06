0daeebbfb2ac0432d57bed3f4ed154dd6f9c31b7
Wesley Klop <wesley@q42.nl>
2022-05-06 19:23:09 +0200

    Try to draw text when a winner exists

--- src/system/end_game.rs
+++ src/system/end_game.rs
@@ -2,12 +2,37 @@ use bevy::prelude::*;
 
 use crate::models::player::Player;
 
-pub fn end_game(_commands: Commands, player_query: Query<(Entity, &Player), With<Player>>) {
+#[derive(Component)]
+struct FinishText;
+
+pub fn end_game(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    player_query: Query<(Entity, &Player), With<Player>>,
+) {
     if player_query.iter().count() != 1 {
         return;
     }
 
-    let (_, winner) = player_query.iter().next().unwrap();
-    info!("Winner!!!");
-    dbg!(winner);
+    let (entity, winner) = player_query.iter().next().unwrap();
+    let font = asset_server.load("fonts/Roboto-Regular.ttf");
+    commands
+        .spawn_bundle(Text2dBundle {
+            text: Text::with_section(
+                match winner {
+                    Player::One => "Player 1 wins!",
+                    Player::Two => "Player 2 wins!",
+                    _ => "Draw!",
+                },
+                TextStyle {
+                    font,
+                    font_size: 60.0,
+                    color: Color::WHITE,
+                },
+                Default::default(),
+            ),
+            ..default()
+        })
+        .insert(FinishText);
+    commands.entity(entity).despawn();
 }
