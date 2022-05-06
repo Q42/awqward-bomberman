7750b88a365cb2c884272c6e5ebf3d09a9734025
Jimmy Aupperlee <jimmy@q42.nl>
2022-05-06 13:00:15 +0200

    Added velocity collision fix

--- src/system/collision.rs
+++ src/system/collision.rs
@@ -1,6 +1,6 @@
-use bevy::{prelude::*, sprite::collide_aabb::collide};
+use bevy::{prelude::*, sprite::collide_aabb::{collide, Collision}};
 
-use crate::models::player::Player;
+use crate::{models::player::Player, TIME_STEP};
 
 #[derive(Component)]
 pub struct Collider;
@@ -11,31 +11,53 @@ pub struct CollisionEvent;
 #[derive(Component, Deref, DerefMut)]
 pub struct Velocity(Vec2);
 
+pub fn apply_velocity(mut query: Query<(&mut Transform, &Velocity)>) {
+    for (mut transform, velocity) in query.iter_mut() {
+        transform.translation.x += velocity.x * TIME_STEP;
+        transform.translation.y += velocity.y * TIME_STEP;
+    }
+}
+
 pub fn check_for_collisions(
-    mut commands: Commands,
-    mut player_query: Query<&Transform, With<Player>>,
-    collider_query: Query<(&Transform, Option<&Player>), With<Collider>>,
+    mut player_query: Query<(&mut Velocity, &Transform), With<Player>>,
+    collider_query: Query<&Transform, With<Collider>>,
     mut collision_events: EventWriter<CollisionEvent>,
 ) {
-    let (player_transform) = player_query.single_mut();
+    let (mut player_velocity, player_transform) = player_query.single_mut();
     let player_size = player_transform.scale.truncate();
 
-    // check collision with walls
-    for (transform, maybe_player) in collider_query.iter() {
+    for transform in collider_query.iter() {
         let collision = collide(
             player_transform.translation,
             player_size,
             transform.translation,
             transform.scale.truncate(),
         );
-        
-        if collision.is_some() {
+
+        if let Some(collision) = collision {
             // Sends a collision event so that other systems can react to the collision
             collision_events.send_default();
 
-            // Bricks should be despawned and increment the scoreboard on collision
-            if maybe_player.is_some() {
-                println!("player is colliding")
+            // reflect the ball when it collides
+            let mut reflect_x = false;
+            let mut reflect_y = false;
+
+            match collision {
+                Collision::Left => reflect_x = player_velocity.x > 0.0,
+                Collision::Right => reflect_x = player_velocity.x < 0.0,
+                Collision::Top => reflect_y = player_velocity.y < 0.0,
+                Collision::Bottom => reflect_y = player_velocity.y > 0.0,
+                Collision::Inside => { /* do nothing */ }
+            }
+
+            // Stop the x velocity if the player hits something on the x axis
+            if reflect_x {
+                player_velocity.x = 0.0;
+            }
+
+            // Stop the y velocity if the player hits something on the y axis
+            if reflect_y {
+                player_velocity.y = 0.0;
             }
         }
     }
