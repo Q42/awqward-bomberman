99b428773dd9079e44750babb0c33d092a0c6ffc
djohalo2 <djovanni@q42.nl>
2022-05-06 11:34:06 +0200

    Add actual level layout

--- src/system/setup.rs
+++ src/system/setup.rs
@@ -5,6 +5,11 @@ use crate::models::player::Player;
 #[derive(Component)]
 struct Wall;
 
+const E: usize = 0; // Edge
+const G: usize = 7; // Grass
+const S: usize = 4; // Shaded grass
+const W: usize = 8; // Wall
+
 pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut texture_atlases: ResMut<Assets<TextureAtlas>>,) {
   let texture_handle = asset_server.load("sprites/stage.png");
   let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(16.0, 16.0), 3, 3);
@@ -13,9 +18,21 @@ pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut texture
   commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
   let level = [
-      [0, 0, 0, 0],
-      [0, 7, 0, 0],
-      [0, 0, 0, 0],
+      [E, E, E, E, E, E, E, E, E, E, E, E, E, E, E],
+      [E, S, S, S, S, S, S, S, S, S, S, S, S, S, E],
+      [E, G, W, G, W, G, W, G, W, G, W, G, W, G, E],
+      [E, G, G, G, G, G, G, G, G, G, G, G, G, G, E],
+      [E, G, W, G, W, G, W, G, W, G, W, G, W, G, E],
+      [E, G, G, G, G, G, G, G, G, G, G, G, G, G, E],
+      [E, G, W, G, W, G, W, G, W, G, W, G, W, G, E],
+      [E, G, G, G, G, G, G, G, G, G, G, G, G, G, E],
+      [E, G, W, G, W, G, W, G, W, G, W, G, W, G, E],
+      [E, G, G, G, G, G, G, G, G, G, G, G, G, G, E],
+      [E, G, W, G, W, G, W, G, W, G, W, G, W, G, E],
+      [E, G, G, G, G, G, G, G, G, G, G, G, G, G, E],
+      [E, G, W, G, W, G, W, G, W, G, W, G, W, G, E],
+      [E, G, G, G, G, G, G, G, G, G, G, G, G, G, E],
+      [E, E, E, E, E, E, E, E, E, E, E, E, E, E, E],
   ];
 
   for (row_index, row) in level.iter().enumerate() {
