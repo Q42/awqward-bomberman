dd458949ab05162649a637b2dd2ba8104f976633
Jimmy Aupperlee <jimmy@q42.nl>
2022-05-06 18:33:34 +0200

    more stuff

--- src/system/explosion_expand.rs
+++ src/system/explosion_expand.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::{Res, Query, With, Entity, Commands};
 use bevy_rapier2d::plugin::RapierContext;
 
-use super::explode_bomb::{Explosion, ExplosionCore};
+use crate::models::explosion::ExplosionCore;
 
 pub fn explosion_expand(mut commands: Commands, explosion_query: Query<Entity, With<ExplosionCore>>, rapier_context: Res<RapierContext>) {
     for explosion_entity in explosion_query.iter() {
