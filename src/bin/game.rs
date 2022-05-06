efd442013791e3324e1c048e7776921ccc474b5d
Wesley Klop <wesley@q42.nl>
2022-05-06 10:12:01 +0200

    Split logic into modules

--- src/bin/game.rs
+++ src/bin/game.rs
@@ -1,4 +1,6 @@
-use bevy::{prelude::*, reflect::TypeRegistry};
+use awqward_bomberman::system::scene::*;
+
+use bevy::{prelude::*, DefaultPlugins};
 use color_eyre::Report;
 
 /// This example illustrates loading and saving scenes from files
@@ -21,36 +23,4 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         texture: asset_server.load("sprites/bomberman.png"),
         ..default()
     });
-}
-
-fn load_scene_system(asset_server: Res<AssetServer>, mut scene_spawner: ResMut<SceneSpawner>) {
-    // Scenes are loaded just like any other asset.
-    let scene_handle: Handle<DynamicScene> = asset_server.load("scenes/load_scene_example.scn.ron");
-
-    // SceneSpawner can "spawn" scenes. "Spawning" a scene creates a new instance of the scene in
-    // the World with new entity ids. This guarantees that it will not overwrite existing
-    // entities.
-    scene_spawner.spawn_dynamic(scene_handle);
-
-    // This tells the AssetServer to watch for changes to assets.
-    // It enables our scenes to automatically reload in game when we modify their files
-    asset_server.watch_for_changes().unwrap();
-}
-
-fn save_scene_system(world: &mut World) {
-    // Scenes can be created from any ECS World. You can either create a new one for the scene or
-    // use the current World.
-    let mut scene_world = World::new();
-    scene_world.spawn().insert_bundle((Transform::identity(),));
-    scene_world.spawn();
-
-    // The TypeRegistry resource contains information about all registered types (including
-    // components). This is used to construct scenes.
-    let type_registry = world.resource::<TypeRegistry>();
-    let scene = DynamicScene::from_world(&scene_world, type_registry);
-
-    // Scenes can be serialized like this:
-    info!("{}", scene.serialize_ron(type_registry).unwrap());
-
-    // TODO: save scene
 }
