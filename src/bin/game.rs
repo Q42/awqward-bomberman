6c8076861b3c80941a0792187789b6b3f20939af
Wesley Klop <wesley@q42.nl>
2022-05-06 10:24:13 +0200

    Add input systems

--- src/bin/game.rs
+++ src/bin/game.rs
@@ -1,5 +1,8 @@
-use awqward_bomberman::system::scene::*;
-use awqward_bomberman::system::setup::*;
+use awqward_bomberman::system::{
+    input::{gamepad_system, keyboard_input_system},
+    scene::*,
+    setup,
+};
 
 use bevy::{prelude::*, DefaultPlugins};
 use color_eyre::Report;
@@ -12,7 +15,9 @@ fn main() -> Result<(), Report> {
         .add_plugins(DefaultPlugins)
         .add_startup_system(save_scene_system.exclusive_system())
         .add_startup_system(load_scene_system)
-        .add_startup_system(setup)
+        .add_startup_system(setup::setup)
+        .add_system(keyboard_input_system)
+        .add_system(gamepad_system)
         .run();
 
     Ok(())
